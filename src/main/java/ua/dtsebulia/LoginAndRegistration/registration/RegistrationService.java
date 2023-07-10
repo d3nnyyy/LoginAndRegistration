d3f51d1fc960df91f59ba5b3670c5e50bd43c8e0
d3nnyyy <d3nnn41k@gmail.com>
2023-07-10 07:58:38 +0300

    added registration starter classes

---
+++ src/main/java/ua/dtsebulia/LoginAndRegistration/registration/RegistrationService.java
@@ -0,0 +1,9 @@
+package ua.dtsebulia.LoginAndRegistration.registration;
+
+public class RegistrationService {
+
+
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}

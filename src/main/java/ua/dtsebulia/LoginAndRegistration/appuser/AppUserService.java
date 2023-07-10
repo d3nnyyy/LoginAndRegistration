b90ccf99ac6905dcf91dc42c58aed71c5fc4bcac
d3nnyyy <d3nnn41k@gmail.com>
2023-07-10 08:39:27 +0300

    finished the registration

--- src/main/java/ua/dtsebulia/LoginAndRegistration/appuser/AppUserService.java
+++ src/main/java/ua/dtsebulia/LoginAndRegistration/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
 
     @Override
@@ -19,4 +21,23 @@ public class AppUserService implements UserDetailsService {
                 () -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email))
         );
     }
+
+    public String signUpUser(AppUser appUser) {
+
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+        if (userExists) {
+            throw new IllegalStateException("email is already taken");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        //TODO: Send confirmation token;
+
+
+        return "it works";
+    }
+
 }
